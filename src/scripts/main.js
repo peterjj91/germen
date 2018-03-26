98c6f430b50a5d921229194d74befd25218bc192
ppopeskul <pasha.9191@mail.ru>
2018-03-26 17:40:46 +0300

    main mob

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
-// $(function () {
-//
-// });
+$(document).ready(function() {
+    $('.btn__loading').click(function () {
+        $(this).toggleClass('btn__loading--active')
+    });
+});
+
+$(document).ready(function() {
+    $('.slider-review').slick({
+
+    });
+});
+
+
+$(document).ready(function() {
+    $('#right-menu').sidr({
+        name: 'sidr-right',
+        side: 'right',
+        source: '#header-nav',
+        onOpen: function() {
+            $('.mobile-menu').addClass('active');
+        },
+        onClose: function() {
+            $('.mobile-menu').removeClass('active');
+        }
+    });
+});
+
+$(document).mouseup(function (e){
+    var container = $("#sidr-right");
+
+    if (!container.is(e.target)
+        && container.has(e.target).length === 0) {
+        $.sidr('close', 'sidr-right');
+    }
+});
