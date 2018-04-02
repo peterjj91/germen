06468324c1b2167b7ba67226e282a5e88908d77a
ppopeskul <pasha.9191@mail.ru>
2018-04-02 18:53:04 +0300

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -84,3 +84,23 @@ $(document).ready(function() {
 $(document).ready(function() {
     $('.promo-tooltip').tooltip();
 });
+
+$(document).ready(function() {
+    $('#add_vase').change(function(){
+        if($(this).is(":checked")) {
+            $('.promo-order__optional').addClass("promo-order__optional--active");
+        } else {
+            $('.promo-order__optional').removeClass("promo-order__optional--active");
+        }
+    });
+});
+
+$(document).ready(function() {
+    $('#add_comment').change(function(){
+        if($(this).is(":checked")) {
+            $('.promo-order__comment').addClass("promo-order__comment--active");
+        } else {
+            $('.promo-order__comment').removeClass("promo-order__comment--active");
+        }
+    });
+});
